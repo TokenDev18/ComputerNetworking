353aa009d14f87c49b879ad3e17a54918756ac14
Aaron <tizzle@Aarons-MacBook-Pro.local>
2021-09-09 19:48:25 -0400

    Updated solution.py code

--- solution.py
+++ solution.py
@@ -8,13 +8,13 @@ def welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = 'no'
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = 'yes'
-    elif question == "n Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
+        answer = 'no'
+    elif question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = 'mtls'
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = 'yes'
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
-        answer = 'yes'
+        answer = 'no'
     elif question == "What is the MD5 hashing value to the following message: NYU Computer Networking":
         answer = '42b76fe51778764973077a5a94056724'
     elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
@@ -23,8 +23,6 @@ def welcome_assignment_answers(question):
         answer = 3
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
         answer = 4
-    else:
-        answer = "I cannot debug the question you entered, as it does not exist"
 
     return(answer)
 # Complete all the questions.
@@ -32,5 +30,6 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
     #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
+    debug_question = input()
+    #debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
