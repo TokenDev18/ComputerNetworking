00a4cfbe795fde6004ba4da793ce831f19d1edde
Aaron <tizzle@b-10-27-78-163.dynapool.vpn.nyu.edu>
2021-10-04 22:25:28 -0400

    SMTP Code

--- solution.py
+++ solution.py
@@ -14,8 +14,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     clientSocket.connect((mailserver,port))
     recv = clientSocket.recv(1024).decode()
     #print(recv)
-
-    if recv[:3] != '220':
+    #if recv[:3] != '220':
         #print('220 reply not received from server.')
 
     # Send HELO command and print server response.
@@ -23,7 +22,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     clientSocket.send(heloCommand.encode())
     helo_recv = clientSocket.recv(1024).decode()
     #print(helo_recv)
-    if helo_recv[:3] != '250':
+    #if helo_recv[:3] != '250':
         #print('250 reply not received from server.')
 
     # Send MAIL FROM command and print server response.
